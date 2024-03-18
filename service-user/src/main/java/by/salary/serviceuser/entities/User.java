60adbbe991e657f4702978d03e9ba3fa5a0a4995
Denis Talalaev <71027278+DenisTalalaev@users.noreply.github.com>
2024-03-18 19:47:46 +0300

    User and organisation entities

---
+++ service-user/src/main/java/by/salary/serviceuser/entities/User.java
@@ -0,0 +1,28 @@
+package by.salary.serviceuser.entities;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.math.BigInteger;
+
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class User {
+
+    private BigInteger id;
+    private String userFirstName;
+    private String userSurname;
+    private String userSecondName;
+    private String userLogin;
+    private String userPassword;
+    private String userEmail;
+    private Boolean userIsBlocked;
+    private Boolean userIsVerified;
+    private BigInteger userOrganisationId;
+}
