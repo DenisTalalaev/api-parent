d92f60dcfca6f951a4d09ed059c55776d536cb6d
Denis Talalaev <71027278+DenisTalalaev@users.noreply.github.com>
2024-03-18 19:01:07 +0300

    Fix naming

--- service-auth/src/main/java/by/salary/serviceauth/ServiceAuthApplication.java
+++ service-auth/src/main/java/by/salary/serviceauth/ServiceAuthApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ServerAuthApplication {
+public class ServiceAuthApplication {
 
     public static void main(String[] args) {
 
-        SpringApplication.run(ServerAuthApplication.class, args);
+        SpringApplication.run(ServiceAuthApplication.class, args);
     }
 }
