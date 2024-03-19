bac2fa6b10a88f688a3e72d6ef53181bc9cf1518
Denis Talalaev <71027278+DenisTalalaev@users.noreply.github.com>
2024-03-19 16:40:04 +0300

    User DTO + repository

--- service-user/src/main/java/by/salary/serviceuser/entities/User.java
+++ service-user/src/main/java/by/salary/serviceuser/entities/User.java
@@ -1,11 +1,13 @@
 package by.salary.serviceuser.entities;
 
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import jakarta.persistence.*;
+import lombok.*;
+import org.springframework.beans.factory.annotation.Value;
 
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 import java.math.BigInteger;
 
 /**
@@ -29,17 +31,51 @@ User=
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
+@Entity
+@Data
 public class User {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private BigInteger id;
+
+    @Size(min = 2, max = 50)
     private String userFirstName;
+
+    @Size(min = 2, max = 50)
     private String userSurname;
+
+    @Size(min = 2, max = 50)
     private String userSecondName;
+
+    @Size(min = 8, max = 50)
+    @Column(unique = true)
     private String userLogin;
+
     private String userPassword;
+
+    @Email
     private String userEmail;
+
+    @Value("false")
     private Boolean userIsBlocked;
+
+    @Value("false")
     private Boolean userIsVerified;
+
+    @Value("false")
     private Boolean userIsSignedForNotifications;
+
+    @NotNull
     private BigInteger userOrganisationId;
+
+    public User(String userFirstName, String userSurname, String userSecondName, String userLogin, String userPassword, String userEmail, BigInteger userOrganisationId) {
+        this.userFirstName = userFirstName;
+        this.userSurname = userSurname;
+        this.userSecondName = userSecondName;
+        this.userLogin = userLogin;
+        this.userPassword = userPassword;
+        this.userEmail = userEmail;
+        this.userOrganisationId = userOrganisationId;
+    }
 }
